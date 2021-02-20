0ca74ed266479b280dd060e799426e5f83afc823
SPONGE-JL <dev2sponge@gmail.com>
2021-02-20 03:41:09 +0000

    ch03 constructor overloading

--- ts-basic-sample/src/parts/Person/index.ts
+++ ts-basic-sample/src/parts/Person/index.ts
@@ -3,6 +3,17 @@ import APerson from "./APerson";
 export default class Person extends APerson {
   favorites: string[] = new Array<string>();
 
+  constructor(
+    public name: string,
+    public role: number,
+    favorites: string[],
+    public desc?: string
+  ) {
+    super(name, role, desc);
+    if (favorites && favorites.length > 0)
+      for (let f of favorites) this.favorites.push(f);
+  }
+
   addFavorite(favorite: string) {
     if (!favorite || favorite.length === 0)
       throw new Error("Invalid favorite has insulted.");
