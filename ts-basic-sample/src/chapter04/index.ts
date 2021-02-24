47b9a9c0b3ff036867260f043a7577dd72c553f8
SPONGE-JL <dev2sponge@gmail.com>
2021-02-24 14:55:52 +0000

    ch04 chain method

--- ts-basic-sample/src/chapter04/index.ts
+++ ts-basic-sample/src/chapter04/index.ts
@@ -1,9 +1,11 @@
 import aboutFunction from "./aboutFunction";
 import aboutCallbackFunction from "./aboutCallbackFunction";
+import aboutMethodChain from "./aboutMethodChain";
 
 const explain = (): void => {
   aboutFunction();
   aboutCallbackFunction();
+  aboutMethodChain();
 };
 
 const Chapter04 = {
